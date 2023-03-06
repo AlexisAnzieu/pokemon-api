dd0b3bff3025e3c485ee6521d456af0385a09889
Alexis <11615615+AlexisAnzieu@users.noreply.github.com>
2023-03-06 16:46:35 +0000

    suggest

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,10 +4,13 @@ import { Pokemon, Prisma } from '@prisma/client';
 import { PokemonAlreadyExistsError } from './errors/PokemonAlreadyExists.error';
 import { PokemonDoesntExistError } from './errors/PokemonDoesntExist.error';
 
+// tu peux eviter le premier call "count" en utilisant en catchant l'erreur RecordNotFound
+
 @Injectable()
 export class PokemonService {
   constructor(private prismaService: PrismaService) {}
 
+  // peut etre fusionner create et update avec la function "upsert https://www.prisma.io/docs/concepts/components/prisma-client/crud#update-or-create-records"
   async create(createInput: Prisma.PokemonCreateInput) {
     const count = await this.prismaService.pokemon.count({
       where: { name: createInput.name },
@@ -41,6 +44,8 @@ export class PokemonService {
     where: Prisma.PokemonWhereUniqueInput,
     updateInput: Prisma.PokemonUpdateInput,
   ) {
+    // je pense que update va trigger une erreur tout seul si il ne peut pas supprimer le pokemon
+
     const count = await this.prismaService.pokemon.count({ where });
     if (count === 0) {
       throw new PokemonDoesntExistError();
@@ -53,6 +58,7 @@ export class PokemonService {
   }
 
   async deleteOne(where: Prisma.PokemonWhereUniqueInput) {
+    // je pense que delete va trigger une erreur tout seul si il ne peut pas supprimer le pokemon
     const count = await this.prismaService.pokemon.count({ where });
     if (count === 0) {
       throw new PokemonDoesntExistError();
